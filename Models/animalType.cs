db3d6774703943bff8ea410758ecb5e6e3304b25
vanhallen46 <salamancadiego46@gmail.com>
2024-09-17 07:05:18 -0500

    feat: se configuro la clase animal type para su posterior creacion en la base de datos

--- Models/animalType.cs
+++ Models/animalType.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API_Farm.Models
+
 {
+    [Table("animal_types")]
     public class animalType
     {
+        [Column("id")]
         public int Id {get; set;}
+        [Column("name")]
         public required string Name {get; set;}
+        [Column("description")]
         public string? Description {get; set;}
 
 
