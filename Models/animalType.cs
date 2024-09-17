6eec239a46360fa91f3d74d59b90829b3ee15a2f
vanhallen46 <salamancadiego46@gmail.com>
2024-09-17 07:02:13 -0500

    feat: creacion de la clase animalType, gitgnore, y eliminacion de los archivos predeterminados de la api del clima

---
+++ Models/animalType.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Farm.Models
+{
+    public class animalType
+    {
+        public int Id {get; set;}
+        public required string Name {get; set;}
+        public string? Description {get; set;}
+
+
+    }
+}
